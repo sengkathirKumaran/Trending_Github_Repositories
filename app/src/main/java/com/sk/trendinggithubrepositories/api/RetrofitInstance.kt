a933cf31136ef3752a99bb5cb41038599a703811
sengkathirKumaran <sk7kumaran@gmail.com>
2021-03-01 00:52:12 +0530

    Exception handled.

--- app/src/main/java/com/sk/trendinggithubrepositories/api/RetrofitInstance.kt
+++ app/src/main/java/com/sk/trendinggithubrepositories/api/RetrofitInstance.kt
@@ -1,20 +1,28 @@
 package com.sk.trendinggithubrepositories.api
 
 import com.sk.trendinggithubrepositories.api.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
 
+    private val builder = OkHttpClient.Builder()
+        .callTimeout(2, TimeUnit.MINUTES)
+        .connectTimeout(1, TimeUnit.MINUTES)
+        .readTimeout(1, TimeUnit.MINUTES)
+        .build()
 
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(builder)
             .build()
     }
 
-    val api:GithubService by lazy {
+    val api: GithubService by lazy {
         retrofit.create(GithubService::class.java)
     }
 }
--- app/src/main/java/com/sk/trendinggithubrepositories/repository/GithubRepository.kt
+++ app/src/main/java/com/sk/trendinggithubrepositories/repository/GithubRepository.kt
@@ -2,20 +2,29 @@ package com.sk.trendinggithubrepositories.repository
 
 import android.content.Context
 import com.google.gson.Gson
-import com.sk.trendinggithubrepositories.datastore.LocalDataStore
 import com.sk.trendinggithubrepositories.api.RetrofitInstance
 import com.sk.trendinggithubrepositories.data.RepositoryItem
+import com.sk.trendinggithubrepositories.datastore.LocalDataStore
 import retrofit2.Response
+import java.io.IOException
+import java.net.SocketTimeoutException
 
 class GithubRepository(private val context: Context) {
     lateinit var localDataStore: LocalDataStore
+    private lateinit var response: Response<ArrayList<RepositoryItem>>
 
     private val gson = Gson()
     suspend fun getRepository(): Response<ArrayList<RepositoryItem>> {
-        val response = RetrofitInstance.api.fetchRepos()
-        if (response.isSuccessful) {
-            localDataStore = LocalDataStore(context)
-            localDataStore.save(gson.toJson(response.body()))
+        try {
+            response = RetrofitInstance.api.fetchRepos()
+            if (response.isSuccessful) {
+                localDataStore = LocalDataStore(context)
+                localDataStore.save(gson.toJson(response.body()))
+            }
+        } catch (ex: SocketTimeoutException) {
+            ex.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
         return response
     }
